3f7b5d6ef00c47370b5c23844c7f6a196ac289ad
Tony Panczenko <tony.panczenko@gmail.com>
2022-07-25 23:39:59 +0100

    add and init firebase

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { Quasar } from 'quasar';
 import 'quasar/dist/quasar.css';
+import { initializeApp } from 'firebase/app';
 
 import App from './App.vue';
 import router from './router';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App);
 
